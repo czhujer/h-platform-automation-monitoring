4e9b7f4ec95b342d67e09412d75f89d11c7419a7
Patrik Majer <patrik.majer.pisek@gmail.com>
2020-08-15 18:55:27 +0200

    move jaeger-agent defs into separate file

--- configs-servers/h-prometheus-s1/puppet-docker.pp
+++ configs-servers/h-prometheus-s1/puppet-docker.pp
@@ -8,9 +8,3 @@ class { 'docker':
   docker_ce_package_name      => 'docker',
   docker_group                => 'dockerroot',
 }
-
-docker::run { 'jaeger-agent':
-  image   => 'jaegertracing/jaeger-agent:1.18',
-  command => "--reporter.grpc.host-port=192.168.121.254:14250",
-  ports   => ['6831:6831/udp', '6832:6832/udp'],
-}
